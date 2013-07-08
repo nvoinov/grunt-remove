d223b92cb7e79ef7c527c49abebb0eaa1e40e484
nvoinov <nvoinov.dev@gmail.com>
2013-07-08 18:10:09 +0400

    v0.1.0

--- tasks/remove.js
+++ tasks/remove.js
@@ -8,43 +8,98 @@
 
 'use strict';
 
+var path = require('path'),
+    fs = require('fs');
+
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('remove', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+      var options = this.options();
+      var dirList = this.data.dirList;
+      var fileList = this.data.fileList;
+      var isExists = fs.existsSync || path.existsSync;
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+      var i = 0;
 
-      // Handle options.
-      src += options.punctuation;
+      var delete_file = function(path) {
+          try {
+              fs.unlinkSync(path);
+          } catch(e) {
+              grunt.log.writeln('Unable to delete file: "' + path + '" Message : ' + e.toString());
+          }
+      };
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+      var delete_dir= function(path) {
+          try {
+              fs.rmdirSync(path);
+          } catch(e) {
+              grunt.log.writeln('Unable to delete directory: "' + path + '" Message : ' + e.toString());
+          }
+      };
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+      var isFile = function(path) {
+        return fs.statSync(path).isFile()
+      };
+
+      var isDirectory = function(path) {
+        return fs.statSync(path).isDirectory()
+      };
+
+      var delete_files_in_folder = function (dir) {
+          var iFile = 0;
+          var dFileList, filePath;
+
+          try {
+              dFileList = fs.readdirSync(dir);
+          } catch(e) {
+              grunt.log.writeln('Error read directoty. ' + dir + ' Message : ' + e.toString());
+              return;
+          }
+          if (dFileList.length > 0) {
+              for(iFile = 0; iFile < dFileList.length; iFile += 1) {
+                  filePath = path.resolve(dir, dFileList[iFile]);
+
+                  if (isFile(filePath)) {
+                      grunt.log.writeln('Delete file: ' + filePath);
+                      delete_file(filePath);
+                  } else {
+                      if (isDirectory(filePath)) {
+                          grunt.log.writeln('Delete files in dir: ' + filePath);
+                          delete_files_in_folder(filePath);
+                      }
+                  }
+              }
+          }
+
+          grunt.log.writeln('Delete dir: ' + dir);
+          delete_dir(dir);
+      };
+
+      grunt.log.writeln('Start Delete folders...');
+
+      if (dirList && dirList.length > 0) {
+          for (i = 0; i < dirList.length; i += 1) {
+              if (isExists(dirList[i])) {
+                  delete_files_in_folder(dirList[i]);
+              }
+          }
+      }
+
+      grunt.log.writeln('Start Delete Files...');
+
+      if (fileList && fileList.length > 0) {
+          for (i = 0; i < fileList.length; i += 1) {
+              grunt.log.writeln('Delete File: ' + fileList[i]);
+              if (isExists(fileList[i])) {
+                  if (isFile(fileList[i])) {
+                      delete_file(fileList[i]);
+                  }
+              }
+          }
+      }
   });
 
 };
