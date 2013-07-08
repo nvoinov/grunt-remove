99c5e6b199df3e7adf06d2ba9c4089c5d8abbf24
nvoinov <C:\The Bat!>
2013-07-08 23:48:26 +0400

    v0.1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,62 +12,25 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
+    // Configuration grunt-remove task.
     remove: {
       default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
+          fileList: ['path_to_file_1.extension', 'path_to_file_2.extension'],
+          dirList: ['path_to_dir_1', 'path_to_dir2/']
       },
       custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
+          options: {
+              trace: true
+          },
+          dirList: ['path_to_dir_1', 'path_to_dir2/']
+      }
+    }
   });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'remove', 'nodeunit']);
+  grunt.loadNpmTasks('grunt-contrib-remove');
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['remove']);
 
 };
--- tasks/remove.js
+++ tasks/remove.js
@@ -11,95 +11,103 @@
 var path = require('path'),
     fs = require('fs');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+    grunt.registerMultiTask('remove', 'Remove directories and files.', function () {
+        var options = this.options();
+        var isTrace = options.trace === true ? true : false;
+        var dirList = this.data.dirList;
+        var fileList = this.data.fileList;
+        var isExists = fs.existsSync || path.existsSync;
 
-  grunt.registerMultiTask('remove', 'Your task description goes here.', function() {
-      var options = this.options();
-      var dirList = this.data.dirList;
-      var fileList = this.data.fileList;
-      var isExists = fs.existsSync || path.existsSync;
+        var i = 0;
 
-      var i = 0;
+        var _trace = function (msg) {
+            if (isTrace) {
+                grunt.log.writeln(msg.toString());
+            }
+        };
 
-      var delete_file = function(path) {
-          try {
-              fs.unlinkSync(path);
-          } catch(e) {
-              grunt.log.writeln('Unable to delete file: "' + path + '" Message : ' + e.toString());
-          }
-      };
+        var delete_file = function (path) {
+            try {
+                fs.unlinkSync(path);
+            } catch (e) {
+                grunt.log.writeln('Unable to delete file: "' + path + '" Message : ' + e.toString());
+            }
+        };
 
-      var delete_dir= function(path) {
-          try {
-              fs.rmdirSync(path);
-          } catch(e) {
-              grunt.log.writeln('Unable to delete directory: "' + path + '" Message : ' + e.toString());
-          }
-      };
+        var delete_dir = function (path) {
+            try {
+                fs.rmdirSync(path);
+            } catch (e) {
+                grunt.log.writeln('Unable to delete directory: "' + path + '" Message : ' + e.toString());
+            }
+        };
 
-      var isFile = function(path) {
-        return fs.statSync(path).isFile()
-      };
+        var isFile = function (path) {
+            return fs.statSync(path).isFile()
+        };
 
-      var isDirectory = function(path) {
-        return fs.statSync(path).isDirectory()
-      };
+        var isDirectory = function (path) {
+            return fs.statSync(path).isDirectory()
+        };
 
-      var delete_files_in_folder = function (dir) {
-          var iFile = 0;
-          var dFileList, filePath;
+        var delete_files_in_folder = function (dir) {
+            var iFile = 0;
+            var dFileList, filePath;
 
-          try {
-              dFileList = fs.readdirSync(dir);
-          } catch(e) {
-              grunt.log.writeln('Error read directoty. ' + dir + ' Message : ' + e.toString());
-              return;
-          }
-          if (dFileList.length > 0) {
-              for(iFile = 0; iFile < dFileList.length; iFile += 1) {
-                  filePath = path.resolve(dir, dFileList[iFile]);
+            try {
+                dFileList = fs.readdirSync(dir);
+            } catch (e) {
+                grunt.log.writeln('Error read directory. ' + dir + ' Message : ' + e.toString());
+                return;
+            }
+            if (dFileList.length > 0) {
+                for (iFile = 0; iFile < dFileList.length; iFile += 1) {
+                    filePath = path.resolve(dir, dFileList[iFile]);
 
-                  if (isFile(filePath)) {
-                      grunt.log.writeln('Delete file: ' + filePath);
-                      delete_file(filePath);
-                  } else {
-                      if (isDirectory(filePath)) {
-                          grunt.log.writeln('Delete files in dir: ' + filePath);
-                          delete_files_in_folder(filePath);
-                      }
-                  }
-              }
-          }
+                    if (isFile(filePath)) {
+                        _trace('Delete file: ' + filePath);
+                        delete_file(filePath);
+                    } else {
+                        if (isDirectory(filePath)) {
+                            _trace('Delete files in directory: ' + filePath);
+                            delete_files_in_folder(filePath);
+                        }
+                    }
+                }
+            }
 
-          grunt.log.writeln('Delete dir: ' + dir);
-          delete_dir(dir);
-      };
+            _trace('Delete directory: ' + dir);
+            delete_dir(dir);
+        };
 
-      grunt.log.writeln('Start Delete folders...');
+        _trace('Start Delete directories...');
 
-      if (dirList && dirList.length > 0) {
-          for (i = 0; i < dirList.length; i += 1) {
-              if (isExists(dirList[i])) {
-                  delete_files_in_folder(dirList[i]);
-              }
-          }
-      }
+        if (dirList && dirList.length > 0) {
+            for (i = 0; i < dirList.length; i += 1) {
+                if (isExists(dirList[i])) {
+                    delete_files_in_folder(dirList[i]);
+                }
+            }
+        }
 
-      grunt.log.writeln('Start Delete Files...');
+        _trace('Directories has been deleted.');
+        _trace('Start delete files...');
 
-      if (fileList && fileList.length > 0) {
-          for (i = 0; i < fileList.length; i += 1) {
-              grunt.log.writeln('Delete File: ' + fileList[i]);
-              if (isExists(fileList[i])) {
-                  if (isFile(fileList[i])) {
-                      delete_file(fileList[i]);
-                  }
-              }
-          }
-      }
-  });
+        if (fileList && fileList.length > 0) {
+            for (i = 0; i < fileList.length; i += 1) {
+                _trace('Delete file: ' + fileList[i]);
+                if (isExists(fileList[i])) {
+                    if (isFile(fileList[i])) {
+                        delete_file(fileList[i]);
+                    }
+                }
+            }
+        }
+        _trace('Files has been deleted.');
+    });
 
 };
